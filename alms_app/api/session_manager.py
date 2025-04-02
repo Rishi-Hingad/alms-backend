98cdedb972514c6401d2f500380cd89e482964ef
Saurabh Tiwari <frappe-user@ESMLAP0523>
2025-04-02 16:31:03 +0530

    Changes session_manager  file

--- alms_app/api/session_manager.py
+++ alms_app/api/session_manager.py
@@ -1,8 +1,8 @@
 import frappe
 
-# def add_custom_session_data(login_manager):
-#     if frappe.session.user and frappe.session.user != "Guest":
-#         # designation = frappe.db.get_value("User", frappe.session.user, "designation")
-#         # frappe.local.session.data["designation"] = designation
-#         # print("Session Store",designation)
-#         frappe.local.session_obj.update()
+def add_custom_session_data(login_manager):
+    if frappe.session.user and frappe.session.user != "Guest":
+        # designation = frappe.db.get_value("User", frappe.session.user, "designation")
+        # frappe.local.session.data["designation"] = designation
+        # print("Session Store",designation)
+        frappe.local.session_obj.update()
