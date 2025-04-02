04f44b2debf5c2fc1539a2606b6074ba73aaccba
Saurabh Tiwari <frappe-user@ESMLAP0523>
2025-04-02 15:24:07 +0530

    Remove designation from login -Saurabh

--- alms_app/api/session_manager.py
+++ alms_app/api/session_manager.py
@@ -2,7 +2,7 @@ import frappe
 
 def add_custom_session_data(login_manager):
     if frappe.session.user and frappe.session.user != "Guest":
-        designation = frappe.db.get_value("User", frappe.session.user, "designation")
+        # designation = frappe.db.get_value("User", frappe.session.user, "designation")
         # frappe.local.session.data["designation"] = designation
         # print("Session Store",designation)
         frappe.local.session_obj.update()
