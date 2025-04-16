a7fb5ff88f699c88b847bece6760636f476adf69
Saurabh Tiwari <frappe-user@ESMLAP0523>
2025-04-16 16:49:34 +0530

    chnage in hooks file

--- alms_app/hooks.py
+++ alms_app/hooks.py
@@ -6,8 +6,14 @@ app_email = "rishi.hingad@merillife.com"
 app_license = "mit"
 
 
+import frappe
 
+if frappe.flags.in_migrate or frappe.conf.disable_emails_during_migration:
+    def noop_sendmail(*args, **kwargs):
+        print("[EMAIL BLOCKED]")
+    frappe.sendmail = noop_sendmail
 # on_session_creation  = "alms_app.api.session_manager.add_custom_session_data"
+
 
 
 # Apps
@@ -94,9 +100,6 @@ app_license = "mit"
 #   },
 #   {
 #     "dt": "Workspace Shortcut"
-#   },
-#   {
-#     "dt": "Email Account"
 #   },
 #   {
 #     "dt": "DocType"
