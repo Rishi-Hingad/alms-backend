e7711acfc0959d1843d618f5d90bf8c174cedb20
Saurabh Tiwari <frappe-user@ESMLAP0523>
2025-04-19 16:15:28 +0530

    remove static url, link, mail, password, and changes in UI - Saurabh Tiwari

--- alms_app/crms/doctype/car_quotation/car_quotation.js
+++ alms_app/crms/doctype/car_quotation/car_quotation.js
@@ -3,7 +3,7 @@
 function uploadfile(frm) {
     frm.add_custom_button('Upload File', function () {
         frappe.prompt(
-            { 
+            {
                 label: 'Upload File',
                 fieldname: 'file',
                 fieldtype: 'Attach',
@@ -36,35 +36,35 @@ function uploadfile(frm) {
 
     // Sub-button for sending email
     frm.add_custom_button('Send Modification Quot Email', function () {
-        frappe.msgprint(__('Email sent successfully!')); 
-        send_email(frm.doc.employee_details,"FinanceHead To Quotation Company",{"email_phase":"New","email_send_to":frm.doc.finance_company})
+        frappe.msgprint(__('Email sent successfully!'));
+        send_email(frm.doc.employee_details, "FinanceHead To Quotation Company", { "email_phase": "New", "email_send_to": frm.doc.finance_company })
     }, 'Request Menu')
 
     frm.add_custom_button('Send Revised Quot Email', function () {
-        frappe.msgprint(__('Email sent successfully!')); 
-        send_email(frm.doc.employee_details,"FinanceHead To Quotation Company",{"email_phase":"Revised","email_send_to":frm.doc.finance_company})
+        frappe.msgprint(__('Email sent successfully!'));
+        send_email(frm.doc.employee_details, "FinanceHead To Quotation Company", { "email_phase": "Revised", "email_send_to": frm.doc.finance_company })
     }, 'Request Menu')
 }
 
 
-function setValuesInField(frm,data){
-   
-    const ListOfColumns  = [
-        "finance_company","accessory","gst_and_cess",
-        "employee_details","discount_excluding_gst","insurance",
-        "location","base_price_less_discounts","fleet_management_repairs_and_tyres",
-        "kms","total_discount","24x7_assist",
-        "variant","ex_showroom_amount_net_of_discount","pickup_and_drop",
-        "quote","registration_charges","pickup_and_drop",
-        "interest_rate","residual_value_percent","std_relief_car_non_accdt",
-        "tenure","financed_amount","gst_on_fms",
-        "base_price_excluding_gst","financed_amount","total_emi",
-        "gst","emi_financing","status",
-        "ex_showroom_amount","finance_emi_road_tax"
+function setValuesInField(frm, data) {
+
+    const ListOfColumns = [
+        "finance_company", "accessory", "gst_and_cess",
+        "employee_details", "discount_excluding_gst", "insurance",
+        "location", "base_price_less_discounts", "fleet_management_repairs_and_tyres",
+        "kms", "total_discount", "24x7_assist",
+        "variant", "ex_showroom_amount_net_of_discount", "pickup_and_drop",
+        "quote", "registration_charges", "pickup_and_drop",
+        "interest_rate", "residual_value_percent", "std_relief_car_non_accdt",
+        "tenure", "financed_amount", "gst_on_fms",
+        "base_price_excluding_gst", "financed_amount", "total_emi",
+        "gst", "emi_financing", "status",
+        "ex_showroom_amount", "finance_emi_road_tax"
     ]
 
-    ListOfColumns.forEach(column =>{
-        if (data[column] !== undefined) { 
+    ListOfColumns.forEach(column => {
+        if (data[column] !== undefined) {
             frm.set_value(column, data[column]);
         } else {
             frappe.msgprint(`${column} not found in data`);
@@ -76,7 +76,7 @@ function setValuesInField(frm,data){
 
 
 
-function send_email(user,email_send_to,payload=null){
+function send_email(user, email_send_to, payload = null) {
     frappe.call({
         method: "alms_app.api.emailsService.email_sender",
         args: {
@@ -154,16 +154,16 @@ function FinalSelectedQuotation(frm) {
             filters: {
                 'employee_details': frm.doc.employee_details,
             },
-            limit_page_length: 100 
+            limit_page_length: 100
         },
-        callback: function(response) {
+        callback: function (response) {
             if (response.message && response.message.length > 0) {
                 let rejectedDocs = response.message
                     .map(doc => doc.name)
-                    .filter(name => name !== frm.doc.name); 
-                
+                    .filter(name => name !== frm.doc.name);
+
                 // alert("Rejected Quotations: \n" + rejectedDocs.join("\n"));
-                
+
 
                 rejectedDocs.forEach(docName => {
                     frappe.call({
@@ -177,7 +177,7 @@ function FinalSelectedQuotation(frm) {
                                 finance_team_status: 'Rejected'
                             },
                         },
-                        callback: function(res) {
+                        callback: function (res) {
                             if (!res.exc) {
                                 console.log(`Updated ${docName} to Rejected`);
                             } else {
@@ -201,14 +201,36 @@ function FinalSelectedQuotation(frm) {
 
 function toggleFieldStatus(frm) {
 
-    Object.keys(frm.fields_dict).forEach(function(fieldname) {
-        if (frappe.session.user === "financehead@gmail.com" && fieldname === "finance_head_status") {
-             frm.set_df_property(fieldname, "read_only", 0);
-        } else if (frappe.session.user === "finance@gmail.com" && fieldname === "finance_team_status") {
-            frm.set_df_property(fieldname, "read_only", 0);
-        } else {
-            frm.set_df_property(fieldname, "read_only", 1);
-        }
+    Object.keys(frm.fields_dict).forEach(function (fieldname) {
+        // if (frappe.session.user === "financehead@gmail.com" && fieldname === "finance_head_status") {
+        //      frm.set_df_property(fieldname, "read_only", 0);
+        // } else if (frappe.session.user === "finance@gmail.com" && fieldname === "finance_team_status") {
+        //     frm.set_df_property(fieldname, "read_only", 0);
+        // } else {
+        //     frm.set_df_property(fieldname, "read_only", 1);
+        // }
+
+
+
+        frappe.call({
+            method: "alms_app.crms.doctype.car_indent_form.car_indent_form.management",
+            args: {
+                current_frappe_user: frappe.session.user
+            },
+            callback: function (r) {
+                const userData = r.message;
+                if (userData === "Finance Head" && fieldname === "finance_head_status") {
+                    frm.set_df_property(fieldname, "read_only", 0);
+                } else if (userData === "Finance" && fieldname === "finance_team_status") {
+                    frm.set_df_property(fieldname, "read_only", 0);
+                } else {
+                    frm.set_df_property(fieldname, "read_only", 1);
+                }
+
+
+            }
+        })
+
     });
 }
 
@@ -222,15 +244,15 @@ frappe.ui.form.on('Car Quotation', {
 
 
     },
-    onload: function(frm) {
+    onload: function (frm) {
         frm.set_df_property("status", "read_only", true);
         updateStatus(frm);
         toggleFieldStatus(frm);
         uploadfile(frm);
 
     },
-    
-    finance_head_status:function(frm) {
+
+    finance_head_status: function (frm) {
         if (frm.doc.finance_head_status != "Pending") {
             frappe.prompt([
                 {
@@ -239,29 +261,29 @@ frappe.ui.form.on('Car Quotation', {
                     fieldtype: 'Data',
                     reqd: 1
                 }
-            ], 
-            function(values) {
-                frm.set_value('finance_head_remarks', values.remarks_input);
-                frm.refresh_field('finance_head_remarks');
-                frm.save().then(() => {
-                    frm.set_value("status", "Approved");
-                    send_email(frm.doc.employee_details,"FinanceHead To AccountsTeam",{"quotation_id":frm.doc.name})
-                    FinalSelectedQuotation(frm);
-                });
-            }, 
-            'Remarks Required', 
-            'Submit'
+            ],
+                function (values) {
+                    frm.set_value('finance_head_remarks', values.remarks_input);
+                    frm.refresh_field('finance_head_remarks');
+                    frm.save().then(() => {
+                        frm.set_value("status", "Approved");
+                        send_email(frm.doc.employee_details, "FinanceHead To AccountsTeam", { "quotation_id": frm.doc.name })
+                        FinalSelectedQuotation(frm);
+                    });
+                },
+                'Remarks Required',
+                'Submit'
             );
-            
+
         }
-        else{
+        else {
             frm.save_or_update();
         }
     },
 
 
 
-    finance_team_status: function(frm) {
+    finance_team_status: function (frm) {
         if (frm.doc.finance_team_status != "Pending") {
             frappe.prompt([
                 {
@@ -270,19 +292,19 @@ frappe.ui.form.on('Car Quotation', {
                     fieldtype: 'Data',
                     reqd: 1
                 }
-            ], 
-            function(values) {
-                frm.set_value('finance_team_remarks', values.remarks_input);
-                frm.refresh_field('finance_team_remarks');
-                frm.save().then(() => {
-                    send_email(frm.doc.employee_details,"FinanceTeam To FinanceHead")
-                });
-            }, 
-            'Remarks Required', 
-            'Submit'
+            ],
+                function (values) {
+                    frm.set_value('finance_team_remarks', values.remarks_input);
+                    frm.refresh_field('finance_team_remarks');
+                    frm.save().then(() => {
+                        send_email(frm.doc.employee_details, "FinanceTeam To FinanceHead")
+                    });
+                },
+                'Remarks Required',
+                'Submit'
             );
         }
-        else{
+        else {
             frm.save_or_update();
         }
     },
