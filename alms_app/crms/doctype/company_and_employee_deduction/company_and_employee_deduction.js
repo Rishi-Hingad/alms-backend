be4cd63e94156788f4913e412e172b11530f200a
Imranshaikh9799 <tanushree@DESKTOP-MVH9THC.>
2025-06-13 11:56:22 +0000

    Debudtion

--- alms_app/crms/doctype/company_and_employee_deduction/company_and_employee_deduction.js
+++ alms_app/crms/doctype/company_and_employee_deduction/company_and_employee_deduction.js
@@ -2,62 +2,93 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on("Company and Employee Deduction", {
+    onload(frm){
+        // calculate_emi(frm);
+        // calculate_employee_fields(frm)
+    },
     refresh(frm) {
-        calculate_emi(frm);
+        // calculate_emi(frm);
+        // calculate_employee_fields(frm)
     },
 
-    registration_charges: function(frm){
-        calculate_emi(frm);
-    },
+    // registration_charges: function(frm){
+    //     calculate_emi(frm);
+    //     // calculate_employee_fields(frm)
+    // },
 
-    ex_showroom_amount_net_of_discount: function(frm){
-        calculate_emi(frm);
-    },
+    // ex_showroom_amount_net_of_discount: function(frm){
+    //     calculate_emi(frm);
+    //     // calculate_employee_fields(frm)
+    // },
 
-    interest_rate: function(frm) {
-        calculate_emi(frm);
-    },
+    // interest_rate: function(frm) {
+    //     calculate_emi(frm);
+    //     // calculate_employee_fields(frm)
+    // },
 
-    tenure: function(frm) {
-        calculate_emi(frm);
-    },
+    // tenure: function(frm) {
+    //     calculate_emi(frm);
+    //     // calculate_employee_fields(frm)
+    // },
 
-    financed_amount: function(frm) {
-        calculate_emi(frm);
-    },
+    // financed_amount: function(frm) {
+    //     calculate_emi(frm);
+    //     // calculate_employee_fields(frm)
+    // },
 
-    residual_value: function(frm) {
-        calculate_emi(frm);
-    },
+    // residual_value: function(frm) {
+    //     calculate_emi(frm);
+    //     // calculate_employee_fields(frm)
+    // },
 
-    gst: function(frm) {
-        calculate_emi(frm);
-    },
+    // gst: function(frm) {
+    //     calculate_emi(frm);
+    //     // calculate_employee_fields(frm)
+    // },
 
-    insurance: function(frm) {
-        calculate_emi(frm);
-    },
+    // insurance: function(frm) {
+    //     calculate_emi(frm);
+    //     // calculate_employee_fields(frm)
+    // },
 
-    fleet_management_repairs_and_tyres: function(frm) {
-        calculate_emi(frm);
-    },
+    // fleet_management_repairs_and_tyres: function(frm) {
+    //     calculate_emi(frm);
+    //     // calculate_employee_fields(frm)
+    // },
 
-    assist: function(frm) {
-        calculate_emi(frm);
-    },
+    // assist: function(frm) {
+    //     calculate_emi(frm);
+    //     // calculate_employee_fields(frm)
+    // },
 
-    pickup_and_drop: function(frm) {
-        calculate_emi(frm);
-    },
+    // pickup_and_drop: function(frm) {
+    //     calculate_emi(frm);
+    //     // calculate_employee_fields(frm)
+    // },
 
-    std_relief_car_non_accdt: function(frm) {
-        calculate_emi(frm);
-    },
-    
-    gst_and_cess_percent: function(frm) {
+    // std_relief_car_non_accdt: function(frm) {
+    //     calculate_emi(frm);
+    //     // calculate_employee_fields(frm)
+    // },
+    finance_quotation_id: function(frm){
         calculate_emi(frm);
     }
+
+    
 });
+
+
+function PMT(rate, nper, pv, fv = 0, type = 0) {
+ if (rate === 0) return -(pv + fv) / nper;
+ const pow = Math.pow(1 + rate, nper);
+ let pmt = -(
+ (pv * pow + fv) * rate / (pow - 1)
+ );
+ if (type === 1) pmt /= (1 + rate);
+ return pmt;
+}
+
+
 function calculate_emi(frm) {
     const interest_rate = (frm.doc.interest_rate || 0) / 100;  // Annual interest rate as a fraction
     const tenure = frm.doc.tenure || 0;  // Loan tenure in months
@@ -71,67 +102,54 @@ function calculate_emi(frm) {
     const assist = frm.doc.assist || 0;
     const pickup_and_drop = frm.doc.pickup_and_drop || 0;
     const std_relief_car_non_accdt = frm.doc.std_relief_car_non_accdt || 0;
-    const gst_and_cess_percent = frm.doc.gst_and_cess_percent || 0;
 
 
     // Financed amount = Ex-showroom + Registration Charges
     const financed_amount = ex_showroom_amount_net_of_discount + registration_charges;
     console.log('Financed Amount:', financed_amount); // Debugging line
 
-    frm.set_value("financed_amount", financed_amount);
+    frm.set_value("financed_amount", Math.ceil(financed_amount));
 
     // Monthly interest rate (annual interest rate divided by 12)
     const monthly_rate = interest_rate / 12;
-    console.log('Monthly Interest Rate:', monthly_rate); // Debugging line
+    console.log('Monthly Interest Rate:', Math.ceil(monthly_rate)); // Debugging line
 
-    // PMT formula: -Loan amount * Monthly Rate * (1 + Monthly Rate)^Tenure / ((1 + Monthly Rate)^Tenure - 1)
-    let emi = - (financed_amount * monthly_rate * Math.pow(1 + monthly_rate, tenure)) / (Math.pow(1 + monthly_rate, tenure) - 1);
-    console.log('Pre-GST EMI:', emi); // Debugging line
+    
+    const pmt = PMT(monthly_rate, tenure, -financed_amount, residual_value, 1)
+    console.log('pmt:', pmt); // Debugging line
+    const gstPerMonth = gst / tenure;
 
-    // Adjust for annuity due by multiplying by (1 + monthly_rate)
-    emi = emi * (1 + monthly_rate); // Apply the "1" from the Excel formula
-    console.log('Adjusted EMI (Annuity Due):', emi); // Debugging line
+    const emi_financing = pmt - gstPerMonth;
 
-    // Correct EMI adjustment (make it positive)
-    emi = Math.abs(emi); // Make EMI positive to match expected value
-    console.log('Positive EMI (adjusted for sign):', emi); // Debugging line
-
-    // Subtract residual value from EMI (to handle residual value as per the loan terms)
-    const emi_with_residual_adjustment = emi - (residual_value / tenure);
-    console.log('Final EMI (after residual value adjustment):', emi_with_residual_adjustment); // Debugging line
-
-    // Subtract GST adjustment based on formula (C9 / C7)
-    const emi_with_gst_adjustment = emi_with_residual_adjustment - (gst / tenure);
-    console.log('Final EMI (after GST adjustment):', emi_with_gst_adjustment); // Debugging line
-
-    // Round final EMI to 2 decimal places
-    const final_emi = Math.round(emi_with_gst_adjustment * 100) / 100;
-    console.log('Final EMI (Rounded):', final_emi); // Debugging line
-
-    frm.set_value("emi_financing", final_emi);
+    frm.set_value("emi_financing", Math.ceil(emi_financing));
 
     // Additional fields
-    const finance_emi_road_tax = final_emi - (registration_charges / tenure);
+    const finance_emi_road_tax = emi_financing - (registration_charges / tenure);
     console.log('Finance EMI Road Tax:', finance_emi_road_tax); // Debugging line
-    frm.set_value("finance_emi_road_tax", finance_emi_road_tax);
+    frm.set_value("finance_emi_road_tax", Math.ceil(finance_emi_road_tax));
 
-    const cess_percent = gst_and_cess_percent / 100;
-    const gst_and_cess = finance_emi_road_tax * cess_percent;
-    console.log('GST and Cess:', gst_and_cess, cess_percent);
-    frm.set_value("gst_and_cess", gst_and_cess);
+    // const cess_percent = gst_and_cess_percent / 100;
+    const gst_and_cess = finance_emi_road_tax * 45/100;
+    console.log('GST and Cess:', gst_and_cess);
+    frm.set_value("gst_and_cess", Math.ceil(gst_and_cess));
 
     const fms_rate = (18/100)
     const gst_on_fms = (insurance + fleet_management_repairs_and_tyres + assist + pickup_and_drop + std_relief_car_non_accdt) * fms_rate;
     console.log ('gst_on_fms',gst_on_fms);
-    frm.set_value("gst_on_fms",gst_on_fms);
+    frm.set_value("gst_on_fms",Math.ceil(gst_on_fms));
 
-    const total_emi = (final_emi + gst_and_cess + insurance + fleet_management_repairs_and_tyres + assist + pickup_and_drop + std_relief_car_non_accdt + gst_on_fms);
-    console.log("Total EMI", total_emi, final_emi, gst_and_cess, insurance, fleet_management_repairs_and_tyres, assist, pickup_and_drop, std_relief_car_non_accdt, gst_on_fms);
-    frm.set_value("total_emi", total_emi);
+    const total_emi = (emi_financing + gst_and_cess + insurance + fleet_management_repairs_and_tyres + assist + pickup_and_drop + std_relief_car_non_accdt + gst_on_fms);
+    console.log("Total EMI", total_emi, emi_financing, gst_and_cess, insurance, fleet_management_repairs_and_tyres, assist, pickup_and_drop, std_relief_car_non_accdt, gst_on_fms);
+    frm.set_value("total_emi", Math.ceil(total_emi));
 
     const quarterly_payment = total_emi * 3;
     console.log("quarterly_payment", quarterly_payment, total_emi);
-    frm.set_value("quarterly_payment", quarterly_payment);
+    frm.set_value("quarterly_payment", Math.ceil(quarterly_payment));
+
+    
+    const interim_payment = quarterly_payment * 39/90;
+    console.log("interim_payment", interim_payment);
+    frm.set_value("interim_payment", Math.ceil(interim_payment));
     
 
     // Refresh fields
@@ -142,4 +160,85 @@ function calculate_emi(frm) {
     frm.refresh_field("gst_on_fms");
     frm.refresh_field("total_emi");
     frm.refresh_field("quarterly_payment");
+    frm.refresh_field("interim_payment");
+    calculate_employee_fields(frm);
+}
+
+
+function calculate_employee_fields(frm){
+    let employee_ex_showroom_amount_net_of_discount= frm.doc.employee_ex_showroom_amount_net_of_discount || 0;
+    let employee_residual_value_percent= frm.doc.employee_residual_value_percent || 0;
+    let employee_financed_amount=frm.doc.employee_financed_amount || 0;
+    let employee_residual_value=frm.doc.employee_residual_value || 0;
+    let employee_emi_financing=frm.doc.employee_emi_financing || 0;
+    let employee_finance_emi_road_tax=frm.doc.employee_finance_emi_road_tax || 0;
+    let employee_gst_and_cess=frm.doc.employee_gst_and_cess || 0;
+    let employee_insurance=frm.doc.employee_insurance || 0;
+    let employee_fleet_management=frm.doc.employee_fleet_management || 0;
+    let employee_assist=frm.doc.employee_24x7_assist || 0;
+    let employee_pickup_drop=frm.doc.employee_pickup_drop || 0;
+    let employee_std_relief_car_non_accdt= frm.doc.employee_std_relief_car_non_accdt || 0;
+    let employee_gst_on_fms= frm.doc.employee_gst_on_fms || 0;
+    let employee_total_emi=frm.doc.employee_total_emi || 0;
+    let employee_interim_payment=frm.doc.employee_interim_payment || 0;
+    let employee_quarterly_payment= frm.doc.employee_quarterly_payment || 0;
+
+
+    frappe.call({
+        method: "frappe.client.get",
+        args: {
+        doctype: "Car Quotation",
+        name:frm.doc.finance_quotation_id,
+        },
+        callback: function (response) {
+            if(response){
+                console.log("found found");
+                console.log(response);
+                console.log(response.message.ex_showroom_amount_net_of_discount);
+                console.log(frm.doc.ex_showroom_amount_net_of_discount);
+                employee_ex_showroom_amount_net_of_discount= response.message.ex_showroom_amount_net_of_discount - frm.doc.ex_showroom_amount_net_of_discount;
+                frm.set_value("employee_ex_showroom_amount_net_of_discount",Math.ceil(employee_ex_showroom_amount_net_of_discount));
+
+                // employee registration charges field was deleted
+                //initialising as 0 for now
+                const employee_registration_charges=0;
+                employee_financed_amount=employee_ex_showroom_amount_net_of_discount+ employee_registration_charges;
+                frm.set_value("employee_financed_amount",Math.ceil(employee_financed_amount));
+
+                // residual value given as = base price less discounts * residual_value percent
+                const interest_rate = (frm.doc.interest_rate || 0) / 100;  // Annual interest rate as a fraction
+                const tenure = frm.doc.tenure || 0; 
+                const monthly_rate = interest_rate / 12;
+                const gst = frm.doc.gst || 0;
+
+                const pmt= PMT(monthly_rate, tenure,-employee_financed_amount,employee_residual_value,1);
+                const gstPerMonth = gst / tenure;
+                employee_emi_financing = pmt - gstPerMonth;
+                frm.set_value("employee_emi_financing",Math.ceil(employee_emi_financing));
+
+                employee_finance_emi_road_tax=employee_emi_financing-(employee_registration_charges/tenure);
+                frm.set_value("employee_finance_emi_road_tax",Math.ceil(employee_finance_emi_road_tax));
+
+                employee_gst_and_cess=employee_finance_emi_road_tax*45/100;
+                frm.set_value("employee_gst_and_cess",Math.ceil(employee_gst_and_cess));
+
+                employee_total_emi=employee_emi_financing+employee_gst_and_cess+employee_insurance+employee_fleet_management+employee_assist+employee_pickup_drop+employee_std_relief_car_non_accdt+employee_gst_on_fms;
+                frm.set_value("employee_total_emi",Math.ceil(employee_total_emi));
+
+                employee_quarterly_payment=employee_total_emi*3;
+                frm.set_value("employee_quarterly_payment",Math.ceil(employee_quarterly_payment));
+                
+                employee_interim_payment=employee_quarterly_payment*39/90;
+                frm.set_value("employee_interim_payment",Math.ceil(employee_interim_payment));
+
+            }
+            else{
+                console.log("ELEEEEE");
+            }
+        },
+        error: function (err) {
+        console.error("Error fetching list data:", err);
+        },
+    });
+
 }
--- alms_app/crms/doctype/company_and_employee_deduction/company_and_employee_deduction.py
+++ alms_app/crms/doctype/company_and_employee_deduction/company_and_employee_deduction.py
@@ -5,42 +5,4 @@
 from frappe.model.document import Document
 import math
 class CompanyandEmployeeDeduction(Document):
-    def validate(self):
-        # Ensure that all fields are initialized
-        self.interest_rate = self.interest_rate or 0
-        self.tenure = self.tenure or 0
-        self.ex_showroom_amount_net_of_discount = self.ex_showroom_amount_net_of_discount or 0
-        self.registration_charges = self.registration_charges or 0
-        self.residual_value = self.residual_value or 0
-        self.gst = self.gst or 0
-
-        # Calculate financed amount
-        self.financed_amount = self.ex_showroom_amount_net_of_discount + self.registration_charges - self.residual_value
-
-        # Calculate EMI using the PMT formula logic (Excel-style)
-        self.emi_financing = self.calculate_emi(self.financed_amount, self.interest_rate, self.tenure, self.gst)
-
-        # Additional fields
-        self.finance_emi_road_tax = self.calculate_emi_road_tax(self.emi_financing, self.registration_charges, self.tenure)
-        self.gst_and_cess = self.calculate_gst_and_cess(self.finance_emi_road_tax)
-
-    def calculate_emi(self, financed_amount, interest_rate, tenure, gst):
-        # Monthly interest rate (C6/12)
-        monthly_rate = (interest_rate / 100) / 12
-
-        # EMI calculation using the PMT formula logic (Excel-style)
-        emi = -(
-            (financed_amount * monthly_rate * math.pow(1 + monthly_rate, tenure)) /
-            (math.pow(1 + monthly_rate, tenure) - 1)
-        ) + (gst / tenure)  # Subtract GST per month
-        
-        return round(emi, 2)
-
-    def calculate_emi_road_tax(self, emi, registration_charges, tenure):
-        # Calculate the road tax (financing EMI without GST)
-        return emi - (registration_charges / tenure)
-
-    def calculate_gst_and_cess(self, finance_emi_road_tax):
-        # Calculate GST and cess (using a fixed GST interest rate of 45%)
-        gst_interest_rate = 0.45
-        return finance_emi_road_tax * gst_interest_rate
+    pass
