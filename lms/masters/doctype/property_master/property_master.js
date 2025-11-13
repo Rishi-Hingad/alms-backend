9dfc6b47f190e65e7f6d0d26c4f64b7355a3cb8e
Siddhi-67 <siddhi.patel@merillife.com>
2025-11-13 11:27:25 +0000

    Property Master Changes - 13-11-2025

--- lms/masters/doctype/property_master/property_master.js
+++ lms/masters/doctype/property_master/property_master.js
@@ -1,8 +1,12 @@
 // Copyright (c) 2025, Shradha_Siddhi and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Property Master", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Property Master", {
+	type_of_asset: function (frm) {
+		if (frm.doc.type_of_asset == "Property") {
+			frm.set_df_property("city", "reqd", 1);
+			frm.set_df_property("area_sq_ft", "reqd", 1);
+			frm.set_df_property("address", "reqd", 1);
+		}
+	},
+});
