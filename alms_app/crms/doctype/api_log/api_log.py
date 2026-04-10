6586b72e5cf3f71575fe9e731f733cacda4f2f82
Rishi-Hingad <rishi.hingad@merillife.com>
2026-04-10 11:27:02 +0530

    Changes in Vehicle Details Doctyo

--- alms_app/crms/doctype/api_log/api_log.py
+++ alms_app/crms/doctype/api_log/api_log.py
@@ -1,9 +1,14 @@
-# Copyright (c) 2026, Rishi Hingad and contributors
-# For license information, please see license.txt
-
-# import frappe
 from frappe.model.document import Document
-
+from frappe.model.naming import make_autoname
+from frappe.utils import nowdate
 
 class APILog(Document):
-	pass
+
+    def autoname(self):
+        if self.invoice_batch_doc:
+            # LOG-<invoice_batch>-####
+            self.name = make_autoname(f"LOG-{self.invoice_batch_doc}-.####")
+        else:
+            # LOG-YYYYMMDD-####
+            date_str = nowdate().replace("-", "")
+            self.name = make_autoname(f"LOG-{date_str}-.####")
