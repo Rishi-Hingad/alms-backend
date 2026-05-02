eefdb1e3e14e3bb98b0864043dd3ad5a237c8eee
Rishi-Hingad <rishi.hingad@merillife.com>
2026-05-02 11:52:28 +0530

    Fixed all the possible changed occured during the testing on local server

--- alms_app/crms/web_form/car_contract_form/car_contract_form.js
+++ alms_app/crms/web_form/car_contract_form/car_contract_form.js
@@ -1,3 +1,20 @@
-frappe.ready(function() {
-	// bind events here
-})
+frappe.ready(() => {
+
+	const params = new URLSearchParams(window.location.search);
+
+	const user = params.get("user");
+	const company = params.get("company");
+	const quotation = params.get("quotation_form");
+
+	setTimeout(() => {
+
+		if (company) frappe.web_form.set_value("vendor", company);
+		if (quotation) frappe.web_form.set_value("employee_car_process_form", quotation);
+
+		["vendor", "employee_car_process_form"].forEach(field => {
+			frappe.web_form.set_df_property(field, "read_only", 1);
+		});
+
+	}, 800);
+
+});
