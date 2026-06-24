ec50853bef96eefd687355ae1663d1a67fb027c8
Rishi-Hingad <rishi.hingad@merillife.com>
2026-06-24 13:00:00 +0530

    Added calculations in Purchase Form

--- alms_app/crms/doctype/purchase_form/purchase_form.js
+++ alms_app/crms/doctype/purchase_form/purchase_form.js
@@ -1,8 +1,37 @@
 // Copyright (c) 2026, Rishi Hingad and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Purchase Form", {
-// 	refresh(frm) {
+frappe.ui.form.on("Purchase Form", {
+    revised_ex_show_room_price: function(frm) { calculate_revised_fields(frm); },
+    revised_discount: function(frm) { calculate_revised_fields(frm); },
+    revised_tcs: function(frm) { calculate_revised_fields(frm); },
+    revised_registration_charges: function(frm) { calculate_revised_fields(frm); },
+    revised_accessories: function(frm) { calculate_revised_fields(frm); },
+    kilometers_per_year: function(frm) { calculate_total_kilometers(frm); },
+    tenure_in_years: function(frm) { calculate_total_kilometers(frm); }
+});
 
-// 	},
-// });
+function calculate_revised_fields(frm) {
+    let ex_show_room = flt(frm.doc.revised_ex_show_room_price);
+    let discount = flt(frm.doc.revised_discount);
+    let tcs = flt(frm.doc.revised_tcs);
+    let reg_charges = flt(frm.doc.revised_registration_charges);
+    let accessories = flt(frm.doc.revised_accessories);
+
+    let net_ex_show_room = ex_show_room - discount + tcs;
+    let financed_amount = net_ex_show_room + reg_charges + accessories;
+
+    frm.set_value("revised_net_ex_showroom_price", net_ex_show_room);
+    frm.set_value("revised_financed_amount", financed_amount);
+}
+
+function calculate_total_kilometers(frm) {
+    let km_per_year = flt(frm.doc.kilometers_per_year);
+    let tenure = flt(frm.doc.tenure_in_years);
+    
+    if (km_per_year && tenure) {
+        frm.set_value("total_kilometers", km_per_year * tenure);
+    } else {
+        frm.set_value("total_kilometers", "");
+    }
+}
--- alms_app/crms/doctype/purchase_form/purchase_form.py
+++ alms_app/crms/doctype/purchase_form/purchase_form.py
@@ -1,9 +1,24 @@
 # Copyright (c) 2026, Rishi Hingad and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
-
+from frappe.utils import flt
 
 class PurchaseForm(Document):
-	pass
+    def validate(self):
+        # Calculate revised fields if applicable
+        ex_show_room = flt(self.revised_ex_show_room_price)
+        discount = flt(self.revised_discount)
+        tcs = flt(self.revised_tcs)
+        reg_charges = flt(self.revised_registration_charges)
+        accessories = flt(self.revised_accessories)
+
+        if ex_show_room or discount or tcs or reg_charges or accessories:
+            self.revised_net_ex_showroom_price = ex_show_room - discount + tcs
+            self.revised_financed_amount = self.revised_net_ex_showroom_price + reg_charges + accessories
+
+        if self.kilometers_per_year and self.tenure_in_years:
+            self.total_kilometers = flt(self.kilometers_per_year) * flt(self.tenure_in_years)
+        else:
+            self.total_kilometers = 0
