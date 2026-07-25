871c84ed6ccacb08c027993bd4c11e7747a0e855
Rishi-Hingad <rishi.hingad@merillife.com>
2026-07-25 19:30:29 +0530

    Add the akms_app in the branch hook file to get installed

--- leasemanagement/rename_app.py
+++ leasemanagement/rename_app.py
@@ -3,11 +3,21 @@ from frappe.installer import add_to_installed_apps, remove_from_installed_apps
 
 def execute():
     installed_apps = frappe.get_installed_apps()
+    # If leasemanagement is still there (first run)
     if "leasemanagement" in installed_apps:
-        print("Migrating legacy app name 'leasemanagement' to 'lease_app' in tabInstalled Applications...")
+        print("Migrating legacy app name 'leasemanagement' to 'lease_app' and 'alms_app' in tabInstalled Applications...")
         if "lease_app" not in installed_apps:
             add_to_installed_apps("lease_app", rebuild_website=False)
+        if "alms_app" not in installed_apps:
+            add_to_installed_apps("alms_app", rebuild_website=False)
         remove_from_installed_apps("leasemanagement")
-        # Clear cache again to be absolutely sure
         frappe.clear_cache()
-        print("Successfully migrated app name to 'lease_app'.")
+        print("Successfully migrated app name.")
+        return
+
+    # If leasemanagement was already removed in the previous deploy, but alms_app wasn't added
+    if "lease_app" in installed_apps and "alms_app" not in installed_apps:
+        print("Adding 'alms_app' to tabInstalled Applications...")
+        add_to_installed_apps("alms_app", rebuild_website=False)
+        frappe.clear_cache()
+        print("Successfully added alms_app.")
